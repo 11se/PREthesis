b6614546e45354112f09f3e11ba971a1649d3820
Supakorn Yothawuttinan <fnfives@gmail.com>
2023-11-22 20:30:28 +0700

    nothing

--- Pre-Thesis/Assets/MainMenu.cs
+++ Pre-Thesis/Assets/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("URPBK");
+        SceneManager.LoadSceneAsync("CutSceneIntro");
     }
     public void quitGame()
     {
